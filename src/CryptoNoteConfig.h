0696a3c2e740e04be6788b2130b505373184b43e
zircoin <34025329+zircoin@users.noreply.github.com>
2017-11-28 04:20:43 -0500

    Update CryptoNoteConfig.h
    add seednode

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -104,8 +104,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "45.33.108.65:31415",
 };
 
 struct CheckpointData {
